72177ddb0b2e57a4c2115d14b4e0a867bf5e4bfc
jack12324 <jacksporter24@gmail.com>
2023-02-22 21:09:04 -0600

    Added increment function to rename class

--- Rename.py
+++ Rename.py
@@ -10,7 +10,29 @@ def rename_photos(search, replace):
                 new_name = replace+"_"+split[1]
                 os.rename(file, new_name)
 
+def increase_index(search, base):
+    lfiles = os.listdir()
+    for file in lfiles:
+        if search in file:
+            if os.path.isfile(file):
+                year_and_index = file.split("_")
+                index_and_file = year_and_index[1].split(".")
+                num = int(index_and_file[0]) + base
+                new_name = "temp-"+year_and_index[0]+"_"+str(num)+"."+index_and_file[1]
+                os.rename(file, new_name)
+                f.write(new_name)
+                f.write("\n")
+    lfiles = os.listdir()
+    for file in lfiles:
+        print(file)
+        if search in file:
+            print(file)
+            if os.path.isfile(file):
+                print(file)
+                remove_temp = file.split("-")
+                os.rename(file, remove_temp[1])
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    rename_photos("90s", "1990s")
+    rename_photos("10s", "2010s")
