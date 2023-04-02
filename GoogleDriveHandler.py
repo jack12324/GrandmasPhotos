608ac9de9ff0961d64959b65fad33b57f6bb4535
Jack <jacksporter24@gmail.com>
2023-04-02 12:09:58 -0400

    Added folder caching to reduce number of requests made to slow drive api. If a file already exists with the same name in a parent folder, the drive handler will now replace it instead of uploading a duplicate

--- GoogleDriveHandler.py
+++ GoogleDriveHandler.py
@@ -13,8 +13,10 @@ class GoogleDriveHandler(UploadHandler):
     # If modifying these scopes, delete the file token.json.
     SCOPES = ['https://www.googleapis.com/auth/drive']
     FOLDER = 'Grandmas Photos'
+    root_id = None
+    folder_cache = {}
 
-    #Uploads a single image to drive. the upload location will be the app folder path FOLDER plus
+    # Uploads a single image to drive. the upload location will be the app folder path FOLDER plus
     # any path of the original file following the Tif directory. ie. file C:test/foo/Jpg/bar/photo.jpg
     # will be uploaded to FOLDER/bar/photo.jpg on google drive.
     # this function will create any folders it needs along the path
@@ -24,8 +26,9 @@ class GoogleDriveHandler(UploadHandler):
         """
         creds = self.__authenticate()
         (folder_path, file_name) = os.path.split(file)
-        if "Tif\\" in folder_path:
-            file_path = folder_path.split("Jpg/")[1]
+        print(folder_path, ", ", file_name)
+        if "Jpg\\" in folder_path:
+            file_path = folder_path.split("Jpg\\")[1]
         else:
             file_path = ""
         full_path = os.path.join(self.FOLDER, file_path)
@@ -34,6 +37,7 @@ class GoogleDriveHandler(UploadHandler):
             # create drive api client
             service = build('drive', 'v3', credentials=creds)
             parent_folder = self.create_or_get_folder_path(service, full_path)
+            existing_id = self.get_file_id(service, file_name, parent_folder)
 
             file_metadata = {
                 'name': file_name,
@@ -42,16 +46,44 @@ class GoogleDriveHandler(UploadHandler):
             media = MediaFileUpload(file, mimetype='image/jpg',
                                     resumable=True)
             # pylint: disable=maybe-no-member
-            file = service.files().create(body=file_metadata, media_body=media,
-                                          fields='id').execute()
-            print(F'File with ID: "{file.get("id")}" has been uploaded.')
+            if existing_id is None:
+                file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+                print(F'File with ID: "{file.get("id")}" has been uploaded.')
+            else:
+                file = service.files().update(fileId=existing_id, media_body=media).execute()
+                print(F'File with ID: "{file.get("id")}" has been updated.')
 
         except HttpError as error:
             print(F'An error occurred: {error}')
             return None
 
+    def get_file_id(self, service, file_name: str, parent_folder: str) -> str:
+        files = []
+        page_token = None
+        query = "mimeType='image/jpeg' and '{}' in parents and name = '{}' and trashed = false".format(parent_folder,
+                                                                                                       file_name)
+        while True:
+            response = service.files().list(q=query,
+                                            spaces='drive',
+                                            fields='nextPageToken, '
+                                                   'files(id, name)',
+                                            pageToken=page_token).execute()
+            files = response.get('files', [])
+            if len(files) > 0:
+                return files[0].get("id")
+            files.extend(response.get('files', []))
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return None
+
     def create_or_get_folder_path(self, service, folder):
-        parent_folder = service.files().get(fileId='root').execute()['id']
+        if self.root_id is None:
+            parent_folder = service.files().get(fileId='root').execute()['id']
+            self.root_id = parent_folder
+            self.folder_cache[parent_folder] = {}
+        else:
+            parent_folder = self.root_id
         folders = folder.strip("\\").split("\\")
         print(folders)
         while len(folders) > 0:
@@ -65,7 +97,6 @@ class GoogleDriveHandler(UploadHandler):
             return self.create_folder(service, folder, parent_folder)
         else:
             return folder_id
-
 
     # Given a folder path, creates the folder
     # Returns the id of the created folder
@@ -81,12 +112,29 @@ class GoogleDriveHandler(UploadHandler):
                                       ).execute()
         print(F'Created Folder: "{folder}"Folder ID: "{file.get("id")}".')
 
-        return file.get('id')
+        folder_id = file.get('id')
+        self.add_folder_to_cache(folder, folder_id, parent_folder)
 
-    #Takes a path to a folder, returns the id of the folder if it exists, None if it doesn't
+        return folder_id
+
+    def add_folder_to_cache(self, folder, folder_id, parent_folder):
+        if parent_folder in self.folder_cache:
+            self.folder_cache[parent_folder][folder] = folder_id
+        else:
+            self.folder_cache[parent_folder] = {}
+            self.folder_cache[parent_folder][folder] = folder_id
+
+    # Takes a path to a folder, returns the id of the folder if it exists, None if it doesn't
     def get_folder(self, service, folder: str, parent_folder: str = None) -> str:
 
-        query = "mimeType='application/vnd.google-apps.folder' and '{}' in parents and name = '{}' and trashed = false".format(parent_folder, folder)
+        if parent_folder in self.folder_cache:
+            if folder in self.folder_cache[parent_folder]:
+                print(F'Cache Hit: {parent_folder}, {folder}')
+                return self.folder_cache[parent_folder][folder]
+
+        print(F'Cache Miss: {parent_folder}, {folder}')
+        query = "mimeType='application/vnd.google-apps.folder' and '{}' in parents and name = '{}' and trashed = false".format(
+            parent_folder, folder)
         page_token = None
         while True:
             # pylint: disable=maybe-no-member
@@ -98,14 +146,14 @@ class GoogleDriveHandler(UploadHandler):
             files = response.get('files', [])
             if len(files) > 0:
                 print(F'Found file: {files[0].get("name")}, {files[0].get("id")}, {files[0].get("parents")}')
-                return files[0].get("id")
+                folder_id = files[0].get('id')
+                self.add_folder_to_cache(folder, folder_id, parent_folder)
+                return folder_id
             files.extend(response.get('files', []))
             page_token = response.get('nextPageToken', None)
             if page_token is None:
                 break
         return None
-
-
 
     def __authenticate(self) -> Credentials:
         creds = None
